b08c51e95ccccbac0640ad19f324b4c4c37df3d7
Evhorus <sesbas778@gmail.com>
2025-10-11 18:16:59 -0500

    feat: refactor authentication module and add Clerk client provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClerkClientProvider } from './providers/clerk-client.provider';
 import { AuthModule } from './auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
-import { ClerkAuthGuard } from './auth/guards/clerk-auth.guard';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [AuthModule, PrismaModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    ClerkClientProvider,
-    {
-      provide: APP_GUARD,
-      useClass: ClerkAuthGuard,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
-import { ClerkClientProvider } from 'src/providers/clerk-client.provider';
+import { ClerkClientProvider } from 'src/auth/providers/clerk-client.provider';
 import { ClerkStrategy } from './strategies/clerk.strategy';
+import { APP_GUARD } from '@nestjs/core';
+import { ClerkAuthGuard } from './guards/clerk-auth.guard';
 
 @Module({
   imports: [PassportModule],
-  providers: [ClerkStrategy, ClerkClientProvider],
+  providers: [
+    ClerkStrategy,
+    ClerkClientProvider,
+    {
+      provide: APP_GUARD,
+      useClass: ClerkAuthGuard,
+    },
+  ],
   exports: [PassportModule],
 })
 export class AuthModule {}
